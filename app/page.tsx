f91f2324ea127c9d1c986b1dc4a3513a9a17d367
victorsantana03 <viclucigei.123@gmail.com>
2025-08-26 19:53:35 -0300

    chore: update shadcn theme

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "./components/ui/button";
 export default function Home() {
   return (
     <div>
-      <Button variant="ghost">Teste</Button>
+      <Button>Teste</Button>
     </div>
   );
 }
