458eeaac496d43ebb5768e5a9b7c7534a631ea16
victorsantana03 <viclucigei.123@gmail.com>
2025-09-23 20:26:04 -0300

    feat: ensure only authenticated user can book

---
+++ app/_lib/auth.ts
@@ -0,0 +1,23 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { db } from "./prisma";
+import GoogleProvider from "next-auth/providers/google";
+import { AuthOptions } from "next-auth";
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(db),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      session.user = {
+        ...session.user,
+        id: user.id,
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } as any;
+      return session;
+    },
+  },
+};
